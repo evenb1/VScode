7473d797d28b2b0b534f551eddc6af2fac664799
Even <even.russom@strathmore.edu>
2023-05-13 10:40:33 +0300

    third layout and quote

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+
+
+let firstname = "Evan";
+let age = 21;
+
+console.log("Hello", firstname);
+console.log("Hello", firstname);
+console.log("Hello", firstname);
+
+document.getElementById("p1").innerHTML = "Hello" + firstname;
